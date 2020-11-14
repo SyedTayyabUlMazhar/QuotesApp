13ce6142387c5e95e0367d28769750b20ca1aa7f
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2020-11-14 12:07:25 +0500

    loading quotes, upvoting, downvoting and favoriting features implemented. Working on home screen.

--- app/src/main/java/com/magentastudio/quotesapp/Model/Quote.kt
+++ app/src/main/java/com/magentastudio/quotesapp/Model/Quote.kt
@@ -1,12 +1,24 @@
 package com.magentastudio.quotesapp.Model
 
+import com.google.firebase.firestore.DocumentId
+
 class Quote {
-    lateinit var profilePicUrl:String
-    lateinit var username:String
 
-    lateinit var text:String
-    lateinit var author:String
+    @DocumentId
+    lateinit var docId: String
 
-    var upvotes:Long = 0
-    var downvotes:Long = 0
+    lateinit var quote: String
+    lateinit var author: String
+
+    lateinit var user: Map<String, String>
+
+    var votes: Long = 0
+
+    var favorited = false
+    var upvoted = false
+    var downvoted = false
+
+    override fun toString(): String {
+        return "id:$docId user:$user votes:$votes quote:$quote author:$author favorited:$favorited upvoted:$upvoted downvoted:$downvoted"
+    }
 }
